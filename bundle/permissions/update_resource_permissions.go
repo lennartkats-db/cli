3d7c7b678c307b0abe129db9e152ddddc642abc3
Lennart Kats <lennart.kats@databricks.com>
2024-08-22 21:57:19 +0200

    Undo changes related to IS_OWNER

--- bundle/permissions/update_resource_permissions.go
+++ bundle/permissions/update_resource_permissions.go
@@ -7,41 +7,34 @@ import (
 	"strings"
 
 	"github.com/databricks/cli/bundle"
-	"github.com/databricks/cli/bundle/config/resources"
 	"github.com/databricks/cli/libs/diag"
 )
 
 const CAN_MANAGE = "CAN_MANAGE"
 const CAN_VIEW = "CAN_VIEW"
 const CAN_RUN = "CAN_RUN"
-const IS_OWNER = "IS_OWNER"
 
-var allowedLevels = []string{CAN_MANAGE, CAN_VIEW, CAN_RUN, IS_OWNER}
+var allowedLevels = []string{CAN_MANAGE, CAN_VIEW, CAN_RUN}
 var levelsMap = map[string](map[string]string){
 	"jobs": {
-		IS_OWNER:   "IS_OWNER",
 		CAN_MANAGE: "CAN_MANAGE",
 		CAN_VIEW:   "CAN_VIEW",
 		CAN_RUN:    "CAN_MANAGE_RUN",
 	},
 	"pipelines": {
-		IS_OWNER:   "IS_OWNER",
 		CAN_MANAGE: "CAN_MANAGE",
 		CAN_VIEW:   "CAN_VIEW",
 		CAN_RUN:    "CAN_RUN",
 	},
 	"mlflow_experiments": {
-		IS_OWNER:   "CAN_MANAGE",
 		CAN_MANAGE: "CAN_MANAGE",
 		CAN_VIEW:   "CAN_READ",
 	},
 	"mlflow_models": {
-		IS_OWNER:   "CAN_MANAGE",
 		CAN_MANAGE: "CAN_MANAGE",
 		CAN_VIEW:   "CAN_READ",
 	},
 	"model_serving_endpoints": {
-		IS_OWNER:   "CAN_MANAGE",
 		CAN_MANAGE: "CAN_MANAGE",
 		CAN_VIEW:   "CAN_VIEW",
 		CAN_RUN:    "CAN_QUERY",
@@ -81,88 +74,62 @@ func validate(b *bundle.Bundle) error {
 
 func applyForJobs(ctx context.Context, b *bundle.Bundle) {
 	for key, job := range b.Config.Resources.Jobs {
-		job.Permissions = extendPermissions(job.Permissions, convert(
+		job.Permissions = append(job.Permissions, convert(
 			ctx,
 			b.Config.Permissions,
 			job.Permissions,
 			key,
 			levelsMap["jobs"],
-		))
+		)...)
 	}
 }
 
 func applyForPipelines(ctx context.Context, b *bundle.Bundle) {
 	for key, pipeline := range b.Config.Resources.Pipelines {
-		pipeline.Permissions = extendPermissions(pipeline.Permissions, convert(
+		pipeline.Permissions = append(pipeline.Permissions, convert(
 			ctx,
 			b.Config.Permissions,
 			pipeline.Permissions,
 			key,
 			levelsMap["pipelines"],
-		))
+		)...)
 	}
 }
 
 func applyForMlExperiments(ctx context.Context, b *bundle.Bundle) {
 	for key, experiment := range b.Config.Resources.Experiments {
-		experiment.Permissions = extendPermissions(experiment.Permissions, convert(
+		experiment.Permissions = append(experiment.Permissions, convert(
 			ctx,
 			b.Config.Permissions,
 			experiment.Permissions,
 			key,
 			levelsMap["mlflow_experiments"],
-		))
+		)...)
 	}
 }
 
 func applyForMlModels(ctx context.Context, b *bundle.Bundle) {
 	for key, model := range b.Config.Resources.Models {
-		model.Permissions = extendPermissions(model.Permissions, convert(
+		model.Permissions = append(model.Permissions, convert(
 			ctx,
 			b.Config.Permissions,
 			model.Permissions,
 			key,
 			levelsMap["mlflow_models"],
-		))
+		)...)
 	}
 }
 
 func applyForModelServiceEndpoints(ctx context.Context, b *bundle.Bundle) {
 	for key, model := range b.Config.Resources.ModelServingEndpoints {
-		model.Permissions = extendPermissions(model.Permissions, convert(
+		model.Permissions = append(model.Permissions, convert(
 			ctx,
 			b.Config.Permissions,
 			model.Permissions,
 			key,
 			levelsMap["model_serving_endpoints"],
-		))
+		)...)
 	}
-}
-
-func extendPermissions(permissions []resources.Permission, topLevelPermissions []resources.Permission) []resources.Permission {
-	if !includesOwner(permissions) {
-		// Just merge the permissions, don't need to do anything special
-		return append(permissions, topLevelPermissions...)
-	}
-
-	for _, p := range topLevelPermissions {
-		if p.Level == IS_OWNER {
-			// Prefer the owner from the local permission set
-			continue
-		}
-		permissions = append(permissions, p)
-	}
-
-	return permissions
-}
-
-func includesOwner(permissions []resources.Permission) bool {
-	for _, p := range permissions {
-		if p.Level == IS_OWNER {
-			return true
-		}
-	}
-	return false
 }
 
 func (m *updateResourcePermissions) Name() string {
